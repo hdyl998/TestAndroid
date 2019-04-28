b27c22eb0f7f1a078fd2280c9e6d54ecbb6ccf3c
caiyu <123456789a>
2019-04-28 16:52:09 +0800

    TEST

--- app/src/main/java/com/callphone/myapplication/MainActivity.java
+++ app/src/main/java/com/callphone/myapplication/MainActivity.java
@@ -8,22 +8,17 @@ import android.webkit.WebView;
 public class MainActivity extends AppCompatActivity {
 
 
-    MyWebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
-        webView = findViewById(R.id.webView);
-        webView.loadUrl("http://dev-newbp.yixinfa.cn/#/launcher/519046093701");
     }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        webView.onDestory();
     }
 }
